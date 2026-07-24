b20d0828fe6d3a12ad55ba92c12247417b8caa72
implantacoes-implantta <implantacoes@implantta.com.br>
2026-07-24 11:48:47 -0300

    Add files via upload

---
+++ api.teste/src/test/java/api/teste/ApplicationTests.java
@@ -0,0 +1,13 @@
+package api.teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
